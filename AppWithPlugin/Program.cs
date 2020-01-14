a65500a3462f897e8d2fe9c8605a38586583eaf5
v.prokopchuk <vadim.prokopchuk@ocsico.com>
2020-01-14 08:14:13 +0300

    Add project files.

---
+++ AppWithPlugin/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using PluginBase;
+
+namespace AppWithPlugin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string[] pluginPaths = new string[]
+                {
+                    @"HelloPlugin\bin\Debug\netcoreapp3.1\HelloPlugin.dll",
+                    @"JsonPlugin\bin\Debug\netcoreapp3.1\JsonPlugin.dll",
+                };
+
+                IEnumerable<ICommand> commands = pluginPaths.SelectMany(pluginPath =>
+                {
+                    Assembly pluginAssembly = LoadPlugin(pluginPath);
+                    return CreateCommands(pluginAssembly);
+                }).ToList();
+
+                if (args.Length == 0)
+                {
+                    Console.WriteLine("Commands: ");
+                    foreach (var command in commands)
+                    {
+                        Console.WriteLine($"{command.Name}\t - {command.Description}");
+                    }
+                }
+                else
+                {
+                    foreach (string commandName in args)
+                    {
+                        Console.WriteLine($"-- {commandName} --");
+
+                        var command = commands.FirstOrDefault(c => c.Name == commandName);
+                        if (command == null)
+                        {
+                            Console.WriteLine("No such command is known.");
+                            return;
+                        }
+
+                        command.Execute();
+
+                        Console.WriteLine();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        static Assembly LoadPlugin(string relativePath)
+        {
+            // Navigate up to the solution root
+            string root = Path.GetFullPath(Path.Combine(
+                Path.GetDirectoryName(
+                    Path.GetDirectoryName(
+                        Path.GetDirectoryName(
+                            Path.GetDirectoryName(
+                                Path.GetDirectoryName(typeof(Program).Assembly.Location)))))));
+
+            string pluginLocation = Path.GetFullPath(Path.Combine(root, relativePath.Replace('\\', Path.DirectorySeparatorChar)));
+            Console.WriteLine($"Loading commands from: {pluginLocation}");
+            PluginLoadContext loadContext = new PluginLoadContext(pluginLocation);
+            return loadContext.LoadFromAssemblyName(new AssemblyName(Path.GetFileNameWithoutExtension(pluginLocation)));
+        }
+
+        static IEnumerable<ICommand> CreateCommands(Assembly assembly)
+        {
+            int count = 0;
+
+            foreach (Type type in assembly.GetTypes())
+            {
+                if (typeof(ICommand).IsAssignableFrom(type) && Activator.CreateInstance(type) is ICommand result)
+                {
+                    count++;
+                    yield return result;
+                }
+            }
+
+            if (count == 0)
+            {
+                string availableTypes = string.Join(",", assembly.GetTypes().Select(t => t.FullName));
+                throw new ApplicationException(
+                    $"Can't find any type which implements ICommand in {assembly} from {assembly.Location}.\n" +
+                    $"Available types: {availableTypes}");
+            }
+        }
+    }
+}
---
+++ HelloPlugin/HelloCommand.cs
@@ -0,0 +1,17 @@
+using PluginBase;
+using System;
+
+namespace HelloPlugin
+{
+    public class HelloCommand : ICommand
+    {
+        public string Name { get => "hello"; }
+        public string Description { get => "Displays hello message."; }
+
+        public int Execute()
+        {
+            Console.WriteLine("Hello !!!");
+            return 0;
+        }
+    }
+}
